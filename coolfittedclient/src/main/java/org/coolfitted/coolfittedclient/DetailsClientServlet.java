a6123103d12466813304851cc766624c8dfd0d69
Anthony Serrano <agserran@uci.edu>
2017-06-14 01:58:54 -0700

    page linking fix and image displaying plus history

--- coolfittedclient/src/main/java/org/coolfitted/coolfittedclient/DetailsClientServlet.java
+++ coolfittedclient/src/main/java/org/coolfitted/coolfittedclient/DetailsClientServlet.java
@@ -3,12 +3,15 @@ package org.coolfitted.coolfittedclient;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -25,6 +28,37 @@ public class DetailsClientServlet extends HttpServlet{
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		// view history stuff
+		Queue queue;
+		String t = request.getParameter("param");
+		HttpSession session = request.getSession(true);
+		if (session.isNew()){
+            queue = new LinkedList();
+            queue.add(t);
+            session.setAttribute("hatqueue", queue);
+        }
+        else{
+            queue = (LinkedList)session.getAttribute("hatqueue");
+            if (queue == null){// || queue.size() == 5){
+                //queue.remove();
+                queue = new LinkedList();
+                queue.add(t);
+                session.setAttribute("hatqueue",queue);
+            }
+            else if (queue.size() == 5){
+                if(!queue.contains(t)){
+                    queue.remove();
+                    queue.add(t);
+                    session.setAttribute("hatqueue", queue);
+                }
+            }else{
+                if(!queue.contains(t)){
+                   queue.add(t); 
+                }
+                session.setAttribute("hatqueue",queue);
+            }
+        }
 		
 		ClientConfig config = new ClientConfig();
 		Client client = ClientBuilder.newClient(config);
